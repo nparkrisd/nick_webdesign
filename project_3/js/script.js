80fb579eeb57324c6a11ebf06c16f83d00429449
nparkrisd <35076483+nparkrisd@users.noreply.github.com>
2018-01-23 10:10:00 -0500

    Jan 23

--- project_3/js/script.js
+++ project_3/js/script.js
@@ -1,2 +1,16 @@
 $(document).ready(function(){
+  
+  $("body").css("display", "none");
+  
+  $("body").fadeIn(2000);
 
+  $("a.transition").click(function(event){
+        event.preventDefault();
+        linkLocation = this.href;
+        $("body").fadeOut(1000, redirectPage); 
+  });
+
+  function redirectPage() {
+        window.location = linkLocation;
+    }
+});
