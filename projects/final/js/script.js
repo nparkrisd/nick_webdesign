c7b918ed120de28d50c9d293b3d641ad71aba95d
nparkrisd <35076483+nparkrisd@users.noreply.github.com>
2018-01-31 17:56:54 -0500

    jan 31 5:56pm

--- projects/final/js/script.js
+++ projects/final/js/script.js
@@ -1,11 +1,11 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 12,
+        zoom: 9,
         center: {lat: 37.06, lng: -95.68}
     });
 
     var kmlLayer = new google.maps.KmlLayer({
-        url: 'https://nparkrisd.github.io/nick_webdesign/projects/final/js/conflictareas.kml',
+        url: 'https://raw.githubusercontent.com/nparkrisd/nick_webdesign/master/projects/final/js/carea.kml',
         suppressInfoWindows: true,
         map: map
     });
